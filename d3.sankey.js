30d46af39f0d22625c6068846c95f9edf85160ed
dattasiddhartha <43894036+dattasiddhartha@users.noreply.github.com>
2018-11-25 19:01:59 -0800

    Update d3.sankey.js

--- d3.sankey.js
+++ d3.sankey.js
@@ -14,7 +14,7 @@ qSVG.selectAll(".txt")
     .attr("x", 10)
 //     .attr("y", function(d, i) {0,10,20,30,40,50}
     .attr("y", function(d, i) {
-        return 640 + i * 360
+        return 640 + i * 480
     })
     .transition()
     .duration(300000)
