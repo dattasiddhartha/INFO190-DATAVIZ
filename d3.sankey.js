60d9ed4cf2c96b58a296f643ddc3c724e53de678
dattasiddhartha <43894036+dattasiddhartha@users.noreply.github.com>
2018-11-27 10:20:03 -0800

    Update d3.sankey.js

--- d3.sankey.js
+++ d3.sankey.js
@@ -1,3 +1,12 @@
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", "100%")
+  .attr("height", "100%")
+  .call(d3.behavior.zoom().on("zoom", function () {
+    svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+  }))
+  .append("g")
+
 d3.sankey = function() {
   var sankey = {},
       nodeWidth = 24,
